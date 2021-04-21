7ac645cd59fe1bd1891437c25a71e2a9703a9166
Juliana Vitoria <juliana.vitoria@topcode.com.br>
2021-04-21 12:53:52 -0300

    update name project ands imports

--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -1,4 +1,6 @@
-import 'package:devQuiz/shared/models/awnser_model.dart';
+import 'dart:convert';
+
+import 'package:dev_quiz/shared/models/awnser_model.dart';
 
 class QuestionModel {
   final String title;
@@ -8,4 +10,24 @@ class QuestionModel {
       : assert(
           awnsers.length == 4,
         );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'awnsers': awnsers.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory QuestionModel.fromMap(Map<String, dynamic> map) {
+    return QuestionModel(
+      title: map['title'],
+      awnsers: List<AwnserModel>.from(
+          map['awnsers']?.map((x) => AwnserModel.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuestionModel.fromJson(String source) =>
+      QuestionModel.fromMap(json.decode(source));
 }

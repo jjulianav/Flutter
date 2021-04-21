ce06cb00e71926381c20ebfe0fd992e0aa17077c
Juliana Vitoria <juliana.vitoria@topcode.com.br>
2021-04-21 09:21:33 -0300

    quiz question awnser

---
+++ lib/shared/models/user_model.dart
@@ -0,0 +1,11 @@
+class UeserModel {
+  final String name;
+  final String photoURL;
+  final int score;
+
+  UeserModel({
+    required this.name,
+    required this.photoURL,
+    required this.score,
+  });
+}
